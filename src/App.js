12b1b015ff71c09412381264b44ec0c45a6202e9
Singh-csm <csm8107@gmail.com>
2023-05-25 19:34:23 +0530

    update-design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+
 import "./app.css"
 function App() {
  
   const list = ["milk","curd","vegetables","cheese","fruits"]
 
   return (
-<div>
-<h1>{"List of materials to be shoped"}</h1>
+<div className='main'>
+<p className='main'>Lists</p>
+<h1>{"List of materials to be shopped"}</h1>
 <ul style={{fontWeight:"bold"}}>
       {
         list.map(item => <li style={{border:"2px solid red", padding:"12px",borderBlockEndColor:"bisque"}}>{item}</li>)
