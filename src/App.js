dc945d3446ebcc35687875bd0f17dd494ad0283d
Singh-csm <csm8107@gmail.com>
2023-05-25 19:42:38 +0530

    styling

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
 
   return (
 <div className='main'>
-<p className='main'>Lists</p>
+<p className='main' style={{backgroundColor:"goldenrod"}}>Lists</p>
 <h1>{"List of materials to be shopped"}</h1>
-<ul style={{fontWeight:"bold"}}>
+<ul style={{fontWeight:"bold", backgroundColor:"teal"}}>
       {
         list.map(item => <li style={{border:"2px solid red", padding:"12px",borderBlockEndColor:"bisque"}}>{item}</li>)
       }
